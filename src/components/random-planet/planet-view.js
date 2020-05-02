521335e378cd5f2ece96490602a7c9d6edbac7bf
vovBorya <vovbor02@gmail.com>
2020-05-03 00:35:20 +0300

    Created PlanetView component

---
+++ src/components/random-planet/planet-view.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const PlanetView = ({planet}) => {
+
+  const { id, planetName,
+    population, rotationPeriod,
+    diameter } = planet;
+
+  return (
+    <React.Fragment>
+      <img className="planet-image"
+           src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+      <div>
+        <h4>{ planetName }</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{ population }</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{ rotationPeriod }</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span> { diameter } </span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default PlanetView;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './random-planet.css';
 
 import Loader from '../loader';
+import PlanetView from './planet-view';
 
 import SwapiService from "../../services/swapi-service";
 
@@ -36,37 +37,21 @@ export default class RandomPlanet extends Component {
 
   render() {
 
-    const {  planet: {id, planetName,
-                      population, rotationPeriod,
-                      diameter}, loading } = this.state;
+    const { planet, loading } = this.state;
 
-    if(loading) {
+    /*if(loading) {
       return <Loader/>
-    }
+    }*/
+
+    const loader = loading ? <Loader /> : null;
+    const content = !loading ? <PlanetView planet={ planet }/>: null;
 
     return (
       <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-        <div>
-          <h4>{ planetName }</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{ population }</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{ rotationPeriod }</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span> { diameter } </span>
-            </li>
-          </ul>
-        </div>
+        { loader }
+        { content }
       </div>
 
     );
-  }
-}
+  };
+};
