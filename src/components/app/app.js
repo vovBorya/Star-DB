5f6cb569ba44e1193e91f489a4a2d4e7258632e4
vovBorya <vovbor02@gmail.com>
2020-05-06 23:55:38 +0300

    Created Row components to render itemList and itemDetails

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,8 +5,6 @@ import RandomPlanet from '../random-planet';
 
 import './app.css';
 import PeoplePage from "../people-page";
-import ItemList from "../item-list";
-import PersonDetails from "../person-details";
 import SwapiService from "../../services/swapi-service";
 
 export default class App extends Component{
@@ -17,34 +15,8 @@ export default class App extends Component{
     return (
       <div>
         <Header/>
-        {/*<RandomPlanet/>*/}
+        <RandomPlanet/>
         <PeoplePage />
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllPlanets}
-              renderItem={(item) => item.name}
-            />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={7}/>
-          </div>
-        </div>
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllStarships}
-              renderItem={(item) => item.name}
-            />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={8}/>
-          </div>
-        </div>
       </div>
     );
   }
