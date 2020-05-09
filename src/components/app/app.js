aaadbf19c10f326ee9d309e1fe9897adedf3b360
vovBorya <vovbor02@gmail.com>
2020-05-09 18:25:18 +0300

    Removed excess imports

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,24 +2,38 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-
 import './app.css';
 import PeoplePage from "../people-page";
-import SwapiService from "../../services/swapi-service";
-import ItemDetails from "../item-details";
-import { Record } from "../item-details/item-details";
+import {
+  PersonDetails,
+  PersonList,
+  PlanetDetails,
+  PlanetList,
+  StarshipDetails,
+  StarshipList
+} from "../sw-components";
 import Row from "../row";
-import {PersonDetails, PersonList, PlanetDetails, PlanetList, StarshipDetails, StarshipList} from "../sw-components";
 
 export default class App extends Component{
 
   render() {
+
+    const peopleList = (
+      <PersonList >
+      { ({name}) => <span>{name}</span> }
+      </PersonList>
+    );
+
+    const personDetails = (
+      <PersonDetails itemId={3} />
+    )
 
     return (
       <div>
         <Header/>
         {/*<RandomPlanet/>*/}
         {/*<PeoplePage />*/}
+        {/*<Row left={peopleList} right={personDetails} />*/}
         <PersonDetails itemId={3} />
         <PlanetDetails itemId={5} />
         <StarshipDetails itemId={9} />
