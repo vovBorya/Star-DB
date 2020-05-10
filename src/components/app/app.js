327d92e65e10e4f2d1875786eeeba387638cdee7
vovBorya <vovbor02@gmail.com>
2020-05-10 18:38:52 +0300

    Created context: provider, consumer

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,36 +4,22 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import './app.css';
 import PeoplePage from "../people-page";
-import {
-  PersonDetails,
-  PersonList,
-  PlanetDetails,
-  PlanetList,
-  StarshipDetails,
-  StarshipList
-} from "../sw-components";
-import Row from "../row";
+import { SwapiServiceProvider } from "../swapi-service-context";
+import ErrorBoundry from "../error-boundry";
 
 export default class App extends Component{
 
   render() {
-
-    const peopleList = (
-      <PersonList >
-      { ({name}) => <span>{name}</span> }
-      </PersonList>
-    );
-
-    const personDetails = (
-      <PersonDetails itemId={3} />
-    )
-
     return (
-      <div>
-        <Header/>
-        {/*<RandomPlanet/>*/}
-        <PeoplePage />
-      </div>
+      <ErrorBoundry>
+        <SwapiServiceProvider value={}>
+          <div>
+            <Header/>
+            {/*<RandomPlanet/>*/}
+            <PeoplePage />
+          </div>
+        </SwapiServiceProvider>
+      </ErrorBoundry>
     );
   }
 };
