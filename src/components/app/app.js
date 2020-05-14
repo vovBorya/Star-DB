c5c91706696c7f631240c597de9302ba6acd8c7b
vovBorya <vovbor02@gmail.com>
2020-05-14 14:09:25 +0300

    removed extra imports

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,9 +7,6 @@ import PeoplePage from "../people-page";
 import { SwapiServiceProvider } from "../swapi-service-context";
 import ErrorBoundry from "../error-boundry";
 import SwapiService from "../../services/swapi-service";
-import PlanetDetails from "../sw-components/planet-details";
-import StarshipDetails from "../sw-components/starship-details";
-import {PersonList, PlanetList, StarshipList} from "../sw-components";
 
 export default class App extends Component{
 
@@ -21,12 +18,8 @@ export default class App extends Component{
         <SwapiServiceProvider value={this.swapiService}>
           <div>
             <Header/>
-            {/*<RandomPlanet/>*/}
+            <RandomPlanet/>
             <PeoplePage />
-            {/*<PlanetDetails itemId={9} />
-            <StarshipDetails itemId={9} />*/}
-            <PlanetList />
-            <StarshipList />
           </div>
         </SwapiServiceProvider>
       </ErrorBoundry>
