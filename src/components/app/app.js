579b7b95fcdb1c28b24fbf275e658720c0acb406
vovBorya <vovbor02@gmail.com>
2020-05-18 13:33:27 +0300

    Fixed renderPersonName, optional path in Route

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -31,7 +31,7 @@ export default class App extends Component{
                   render={() => <h2>Welcome to StarDB</h2>}
                   exact={true}
                 />
-                <Route path="/people" component={PeoplePage} />
+                <Route path="/people/:id?" component={PeoplePage} />
                 <Route path="/planets" component={PlanetsPage} />
                 <Route path="/starships" exact component={StarshipsPage} />
                 <Route
